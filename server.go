9e617042d4001dbe3d38ab5a114809dc474e67a7
Jorge Luis <norestraint@protonmail.com>
2024-01-28 21:56:59 -0300

    feat: add basic CRUD endpoints and handlers

--- server.go
+++ server.go
@@ -11,9 +11,33 @@ import (
 func main() {
 	app := fiber.New()
 
+	app.Get("/", indexHandler) // Add this
+
+	app.Post("/", postHandler) // Add this
+
+	app.Put("/update", putHandler) // Add this
+
+	app.Delete("/delete", deleteHandler) // Add this
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
+}
+
+func indexHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func postHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func putHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func deleteHandler(c *fiber.Ctx) error {
+	return c.SendString("Hello")
 }
