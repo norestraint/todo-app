de51e2078b46d0198ba5b3cd7b1d2de47f6e7249
Jorge Luis <norestraint@protonmail.com>
2024-01-29 14:10:41 -0300

    refactor: alter order of fn

--- server.go
+++ server.go
@@ -23,6 +23,55 @@ type todo struct {
 	Item string
 }
 
+func indexHandler(c *fiber.Ctx, db *sql.DB) error {
+	var res string
+	var todos []string
+	rows, err := db.Query("SELECT * FROM todos")
+	defer rows.Close()
+	if err != nil {
+		log.Fatalln(err)
+		c.JSON("An error occured")
+	}
+	for rows.Next() {
+		rows.Scan(&res)
+		todos = append(todos, res)
+	}
+	return c.Render("index", fiber.Map{
+		"Todos": todos,
+	})
+}
+
+func postHandler(c *fiber.Ctx, db *sql.DB) error {
+	newTodo := todo{}
+	if err := c.BodyParser(&newTodo); err != nil {
+		log.Printf("An error occured: %v", err)
+		return c.SendString(err.Error())
+	}
+     
+	fmt.Printf("%v", newTodo)
+	if newTodo.Item != "" {
+		_, err := db.Exec("INSERT into todos VALUES ($1)", newTodo.Item)
+		if err != nil {
+			log.Fatalf("An error occured while executing query: %v", err)
+		}
+	}
+
+	return c.Redirect("/")
+}
+
+func putHandler(c *fiber.Ctx, db *sql.DB) error {
+	olditem := c.Query("olditem")
+	newitem := c.Query("newitem")
+	db.Exec("UPDATE todos SET item=$1 WHERE item=$2", newitem, olditem)
+	return c.Redirect("/")
+}
+
+func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
+	todoToDelete := c.Query("item")
+	db.Exec("DELETE from todos WHERE item=$1", todoToDelete)
+	return c.SendString("deleted")
+}
+
 func main() {
 	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
@@ -30,13 +79,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{
 		Views: engine,
@@ -47,76 +89,21 @@ func main() {
 	})
 
 	app.Post("/", func(c *fiber.Ctx) error {
-		return indexHandler(c, db)
+		return postHandler(c, db)
 	})
 
 	app.Put("/update", func(c *fiber.Ctx) error {
-		return indexHandler(c, db)
+		return putHandler(c, db)
 	})
 
 	app.Delete("/delete", func(c *fiber.Ctx) error {
-		return indexHandler(c, db)
+		return deleteHandler(c, db)
 	})
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
-
-	app.Static("/", "./public/")
+	app.Static("/", "./public")
 	log.Fatalln(app.Listen(fmt.Sprintf(":%v", port)))
-}
-
-func indexHandler(c *fiber.Ctx, db *sql.DB) error {
-	var res string
-	var todos []string
-
-	rows, err := db.Query("SELECT * FROM todos")
-	defer rows.Close()
-
-	if err != nil {
-		log.Fatalln(err)
-		c.JSON("An error occured")
-	}
-
-	for rows.Next() {
-		rows.Scan(&res)
-		todos = append(todos, res)
-	}
-
-	return c.Render("index", fiber.Map{
-		"Todos": todos,
-	})
-}
-
-func postHandler(c *fiber.Ctx, db *sql.DB) error {
-	newTodo := todo{}
-	err := c.BodyParser(&newTodo)
-	if err != nil {
-		log.Printf("An error occurred: %v", err)
-		return c.SendString(err.Error())
-	}
-
-	fmt.Printf("%v", newTodo)
-	if newTodo.Item != "" {
-		_, err := db.Exec("INSERT into todos VALUES ($1)", newTodo.Item)
-		if err != nil {
-            log.Fatalf("An error occurred while executing query: %v", err)
-			return c.SendString(err.Error())
-		}
-	}
-    return c.Redirect("/")
-}
-
-func putHandler(c *fiber.Ctx, db *sql.DB) error {
-    olditem := c.Query("olditem")
-    newitem := c.Query("newitem")
-    db.Exec("UPDATE todos SET item=$1 WHERE item=$2", newitem, olditem)
-    return c.Redirect("/")
-}
-
-func deleteHandler(c *fiber.Ctx, db *sql.DB) error {
-    todoToDelete := c.Query("item")
-    db.Exec("DELETE from todos WHERE item=$1", todoToDelete)
-    return c.SendString("deleted")
 }
